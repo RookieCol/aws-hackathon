5a834cd81e464d78963ba165e9379e88bd7fdaa4
chrisarevalo11 <christian.arevalo.fonseca@gmail.com>
2024-05-22 17:12:59 -0500

    🔇 Removes logs

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    sourcemap: false,
   },
   optimizeDeps: {
     esbuildOptions: {
